7b19df8e3ae8db01d6543e40f604c74397bbcfa0
Repaka Shanmukhi <shanmukhirepaka@gmail.com>
2025-04-25 10:01:38 +0000

    changes

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,37 +1,51 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-   "sap/f/library",
+  "sap/f/library",
   "sap/ui/core/mvc/XMLView"
-], (BaseController,fioriLibrary,XMLView) => {
+], (BaseController, fioriLibrary, XMLView) => {
   "use strict";
   var LayoutType = fioriLibrary.LayoutType;
   return BaseController.extend("fcl.controller.App", {
-      onInit() {
+    onInit() {
       this.oEventBus = this.getOwnerComponent().getEventBus();
-      this.oEventBus.subscribe("flexible","setView1",this.setView1,this);
-      this.oEventBus.subscribe("flexible","setView2",this.setView2,this);
+      this.oEventBus.subscribe("flexible", "setView1", this.setView1, this);
+      this.oEventBus.subscribe("flexible", "setView2", this.setView2, this);
       this.oFlexibleColumnLayout = this.byId("FCL");
-      },
-      setView1: function(){
-        this.oFlexibleColumnLayout.setLayout(LayoutType.OneColumn);
-      },
-      setView2: function (){
-        this._loadView({
-          id: "midView",
-          viewName: "fcl.view.View2"
-        }).then(function(View2) {
-          this.oFlexibleColumnLayout.addMidColumnPage(View2);
-          this.oFlexibleColumnLayout.setLayout(LayoutType.TwoColumnsMidExpanded);
-        }.bind(this));
-      },
-      _loadView: function(options) {
-        var mViews = this._mViews = this._mViews || Object.create(null);
-        if (!mViews[options.id]) {
-          mViews[options.id] = this.getOwnerComponent().runAsOwner(function() {
-            return XMLView.create(options);
+    },
+    setView1: function () {
+      this.oFlexibleColumnLayout.setLayout(LayoutType.OneColumn);
+    },
+    setView2: function (sChannel, sEventId, oData) {
+      var oComponent = this.getOwnerComponent();
+      var oODataModel = oComponent.getModel();
+      oODataModel.read("/EmployeeInfoEmergencyContact", {
+        filters: [new sap.ui.model.Filter("EmployeeID", sap.ui.model.FilterOperator.EQ, oData.Data.ID)],
+            success: function (response) {
+          var oJSONModel = new sap.ui.model.json.JSONModel({
+            items: response.results
           });
+          oComponent.setModel(oJSONModel, "EmergencyModel");
+          this._loadView({
+            id: "midView",
+            viewName: "fcl.view.View2"
+          }).then(function (View2) {
+            this.oFlexibleColumnLayout.addMidColumnPage(View2);
+            this.oFlexibleColumnLayout.setLayout(LayoutType.TwoColumnsMidExpanded);
+          }.bind(this));
+        }.bind(this),
+        error: function () {
+          sap.m.MessageToast.show("Error loading data");
         }
-        return mViews[options.id];
+      });
+    },
+    _loadView: function (options) {
+      var mViews = this._mViews = this._mViews || Object.create(null);
+      if (!mViews[options.id]) {
+        mViews[options.id] = this.getOwnerComponent().runAsOwner(function () {
+          return XMLView.create(options);
+        });
       }
+      return mViews[options.id];
+    }
   });
 });
--- controller/View2.controller.js
+++ controller/View2.controller.js
@@ -9,29 +9,11 @@ sap.ui.define([
         onInit() {
             that = this;
             this.oEventBus = this.getOwnerComponent().getEventBus();
-            this.oEventBus.subscribe("flexible", "setView2", this.data, this);
         },
         onClose: function(){
             this.oEventBus.publish("flexible", "setView1");
         },
-        data:function(sChannel, sEventId, oData){
-            var oModel =this.getOwnerComponent().getModel();
-            oModel.read("/EmployeeInfoEmergencyContact",{
-                filters:[
-                    new sap.ui.model.Filter("ContactEmail",sap.ui.model.FilterOperator.EQ, oData.Data.Email)
-                ],
-                success: (response) =>{
-                    var filtered = response.results;
-                    var oModel = new sap.ui.model.json.JSONModel({
-                        items: filtered
-                    });
-                    this.getView().byId("EmgInfo").setModel(oModel)
-                },
-                error: function(){
-                    console.log("error fetch the data")
-                }
-            })
-        },
+ 
         DeleteBtn: function(oEvent)
         {
             var oButton=oEvent.getSource();
@@ -47,8 +29,8 @@ sap.ui.define([
                 {
                     sap.m.MessageToast.show("Cannot delete record");
                 }
-            }) 
-        }, 
+            })
+        },
          onOpenDialog: function()
         {
             if(!this.Personalinfo)
@@ -84,134 +66,4 @@ sap.ui.define([
         },
     });
 });  
-                                                                // To update the Employee Emergency data
-        // UpdateBtn: function(oEvent)
-        // {
-        //     if(!this.update)
-        //     {
-        //         this.update=sap.ui.xmlfragment("fcl.fragments.Update", this)
-        //     }
-        //     this.update.open();
-        //     var oContext = oEvent.getSource().getBindingContext().getObject(); 
-        //     sap.ui.getCore().byId("ContactName_E").setValue(oContext.ContactName);
-        //     sap.ui.getCore().byId("Email_E").setValue(oContext.ContactEmail);
-        //     sap.ui.getCore().byId("Phone_E").setValue(oContext.ContactPhone);
-        //     sap.ui.getCore().byId("Relationship_E").setValue(oContext.Relationship);
-        //     this.update.open();
-        // },
-        // onUpdateDialog: function()
-        // {
-        //     var sName = sap.ui.getCore().byId("ContactName_E").getValue();
-        //     var sMail = sap.ui.getCore().byId("Email_E").getValue();
-        //     var sPhone = sap.ui.getCore().byId("Phone_E").getValue();
-        //     var sRelationship = sap.ui.getCore().byId("Relationship_E").getValue();
-        //         var oUpdatedEmployee = {
-        //             ContactName:sName,
-        //             ContactEmail: sMail,
-        //             ContactPhone: sPhone,
-        //             Relationship: sRelationship
-        //         };
-        //         var oData = this.getOwnerComponent().getModel();
-        //         var updatePath = `/EmployeeInfoEmergencyContact('${sName}')`
-        //         oData.update(updatePath, oUpdatedEmployee,{
-        //             success: function()
-        //             {
-        //             },
-        //         error: function (error) 
-        //         {
-                
-        //         }   
-        //    })
-        //    this.update.close()
-        // },
-        // onCancleDialog: function()
-        // { 
-        //     this.update.close()
-        // },
-        // UpdateBtn: function(oEvent) {
-        //     if (!this.update) {
-        //         this.update = sap.ui.xmlfragment("fcl.fragments.Update", this);
-        //     }
-            
-        //     var oContext = oEvent.getSource().getBindingContext().getObject(); 
-            
-        //     // Save the ID to use during update
-        //     this._currentContactId = oContext.ID;
-        
-        //     sap.ui.getCore().byId("ContactName_E").setValue(oContext.ContactName);
-        //     sap.ui.getCore().byId("Email_E").setValue(oContext.ContactEmail);
-        //     sap.ui.getCore().byId("Phone_E").setValue(oContext.ContactPhone);
-        //     sap.ui.getCore().byId("Relationship_E").setValue(oContext.Relationship);
-            
-        //     this.update.open();
-        // },
-        
-        // onUpdateDialog: function() {
-        //     var sName = sap.ui.getCore().byId("ContactName_E").getValue();
-        //     var sMail = sap.ui.getCore().byId("Email_E").getValue();
-        //     var sPhone = sap.ui.getCore().byId("Phone_E").getValue();
-        //     var sRelationship = sap.ui.getCore().byId("Relationship_E").getValue();
-        
-        //     var oUpdatedEmployee = {
-        //         ContactName: sName,
-        //         ContactEmail: sMail,
-        //         ContactPhone: sPhone,
-        //         Relationship: sRelationship
-        //     };
-        
-        //     var oData = this.getOwnerComponent().getModel();
-        //     var updatePath = `/EmployeeInfoEmergencyContact(guid'${this._currentContactId}')`;
-        //     oData.update(updatePath, oUpdatedEmployee, {
-        //         success: function() {
-        //             sap.m.MessageToast.show("Update successful!");
-        //         },
-        //         error: function(error) {
-        //             sap.m.MessageBox.error("Update failed: " + error.message);
-        //         }
-        //     });
-        
-        //     this.update.close();
-        // },
-        
-        // onCancleDialog: function() {
-        //     this.update.close();
-        // }
-                                                                     // To create the Employee Emergency data
-        // onOpenDialog: function()
-        // {
-        //     if(!that.Personalinfo)
-        //     {
-        //         that.Personalinfo = sap.ui.xmlfragment("fcl.fragments.Personal Info", that);
-        //         that.getView().addDependent(that.Personalinfo);
-        //     }
-        //     that.Personalinfo.open();
-        // },
-        // onSubmit: function(){
-        //     let oEmg = {
-        //         EmployeeID :sap.ui.getCore().byId("eEmployee_Id").getValue(),
-        //         ContactName :sap.ui.getCore().byId("eContactName").getValue(),
-        //         Relationship :sap.ui.getCore().byId("eRelationship").getValue(),
-        //         ContactPhone :sap.ui.getCore().byId("eContactPhone").getValue(),
-        //         ContactEmail: sap.ui.getCore().byId("eContactEmail").getValue()            
-        //     }
-        //     var oModel = that.getOwnerComponent().getModel();
-        //     oModel.create("/EmployeeInfoEmergencyContact",oEmg,{
-        //         success:function(response){
-        
-        //             sap.m.MessageToast.show("successfull");
-        //             oModel.refresh();
-        //         },error:function(error){
-        //             sap.m.MessageToast.show("Error");
-        //             console.log(error);
-        //         }
-        //     })
-        //     that.Personalinfo.close();
-        // },
-     
-        // OnClose: function()
-        // {
-        //     that.Personalinfo.close()
-        // }
-        // });
-        // });
-        
+       
